746bcf46d45cb8cd8cf0a1430a56da292fef3366
ihsandedec <ddcihsan@gmail.com>
2021-06-30 15:09:59 +0300

    Update Kayıt Güncelleme uygulaması eklendi

---
+++ Application07/update.sql
@@ -0,0 +1,10 @@
+-- UPDATE - Kayıt Güncelleme
+
+-- SELECT * FROM shopapp.products;
+-- UPDATE shopapp.products SET Name = 'Iphone 6' Where ıd = 3
+-- UPDATE shopapp.products SET Name = 'Iphone 6', Price = 3500 Where ıd = 3
+-- SET SQL_SAFE_UPDATES = 0;   
+-- UPDATE shopapp.products  SET Price = Price + 200
+-- UPDATE shopapp.products   SET Price = Price * 1.2 WHERE Category = 'Telefon'
+-- UPDATE shopapp.products SET ImageUrl = 'noproduct.jpeg' WHERE ImageUrl IS NULL 
+
