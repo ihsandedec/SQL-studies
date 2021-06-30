21b971abd42d5b7255cacec6aa86194eee86c85c
ihsandedec <ddcihsan@gmail.com>
2021-06-30 14:26:58 +0300

    Insert kayıt ekleme uygulaması eklendi

---
+++ Application06/ınsert.sql
@@ -0,0 +1,6 @@
+
+-- SELECT * FROM shopapp.products;
+-- INSERT INTO shopapp.products (Name,Price,ImageUrl,Category,Description,Stock) values ('Samsung S10',4500,'5.jpeg','Telefon','Çok iyi bir telefon',10);
+-- INSERT INTO shopapp.products (Name,Price,Category) values ('Iphone X',7500,'Telefon');
+ -- select count(*) as adet from shopapp.products
+ -- select count(ImageUrl) as adet from shopapp.products
