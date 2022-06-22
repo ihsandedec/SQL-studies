9be7f22334a28979750ff2699e8a2e957556b795
ihsandedec <ddcihsan@gmail.com>
2022-06-23 00:35:31 +0300

    Order by çalışması eklendi

---
+++ Education02/Application04/order_by.sql
@@ -0,0 +1,11 @@
+--order by numaric veya alfabetik olarak siralama operatoru
+
+Select * from Products
+
+Select * from Products order by ProductName
+
+Select * from Products  order by UnitPrice --ucuzdan pahalýya
+
+Select * from Products  order by UnitPrice desc --pahalýdan ucuza
+
+Select * from Products  order by UnitPrice,ProductName --ucuzdan pahalý sýralandý ayný fiyatta olanlarýn name leri alfabetik sýralandý
