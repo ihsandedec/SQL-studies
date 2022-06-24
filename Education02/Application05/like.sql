358d41fbe99a898aaec2fed3a5c916a8708daa22
ihsandedec <ddcihsan@gmail.com>
2022-06-24 15:34:49 +0300

    like örnek çalışması eklendi

---
+++ Education02/Application05/like.sql
@@ -0,0 +1,7 @@
+select * from Products
+
+select * from Products where ProductName like 'ch%'
+
+select * from Products where ProductName like '%a'
+
+select * from Products where ProductName like '%ch%'
