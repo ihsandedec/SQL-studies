55a87de1b63f0bddec302cbd2572f8562f47bf03
ihsandedec <ddcihsan@gmail.com>
2022-06-25 03:02:35 +0300

    Count için örnek çalışma eklendi

---
+++ Education02/Application07/count.sql
@@ -0,0 +1,7 @@
+select * from Products
+
+Select Count(*) as [Ürün Sayýsý] from Products
+Select Count(p.ProductName)  from Products p
+
+Select Count(*) from Customers 
+Select Count(c.Region) from Customers c --count NULL larý saymaz
